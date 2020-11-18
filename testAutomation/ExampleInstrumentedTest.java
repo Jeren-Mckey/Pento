4b16402af1c0da4deca33a1e5bb1775259c4bf94
Areeb Yaqub <aygohan@gmail.com>
2020-11-18 13:40:35 -0800

    separate file for test automation

---
+++ testAutomation/ExampleInstrumentedTest.java
@@ -0,0 +1,20 @@
+package com.example.pento;
+
+import org.junit.Test;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+
+public class ExampleInstrumentedTest {
+    private Object InstrumentationRegistry;
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        //Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+       // assertEquals("com.example.pento", appContext.getPackageName());
+    }
+}
